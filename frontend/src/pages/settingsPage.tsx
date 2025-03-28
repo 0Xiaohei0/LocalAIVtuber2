6c6256b265391d2d99a963b0f9aa3ec0a9e73612
Xiaohei <15852786007ysc@gmail.com>
2025-03-28 00:39:29 -0400

    settings page

--- frontend/src/pages/settingsPage.tsx
+++ frontend/src/pages/settingsPage.tsx
@@ -1,9 +1,20 @@
+import { Panel } from "@/components/panel"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+
 function SettingsPage() {
     return (
         <div className="p-5">
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Settings</h3>
+            <Panel className="max-w-4xl mx-auto">
+                <div className="flex items-center space-x-2">
+                    <Switch id="load-llm-cpu" />
+                    <Label htmlFor="airplane-mode">Load LLM on CPU</Label>
+                    <p className="text-sm text-muted-foreground">For Reducing load on lower end graphics cards, latency will be increased.</p>
+                </div>
+            </Panel>
         </div>
     )
-  }
-  
-  export default SettingsPage
+}
+
+export default SettingsPage
