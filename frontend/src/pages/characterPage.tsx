b984cca10fd5cda33216f40d968f70189b43031a
Xiaohei <15852786007ysc@gmail.com>
2025-03-30 00:57:24 -0400

    vrm3d model render

--- frontend/src/pages/characterPage.tsx
+++ frontend/src/pages/characterPage.tsx
@@ -1,15 +1,12 @@
-import Live2DCanvas from "@/components/live-2d-renderer"
+import { CharacterRender } from "@/components/character-render"
+
 
 
 function CharacterPage() {
     return (
-        <div className="p-5">
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Character</h3>
-            <div className="w-fill h-fill">
-                {/* <Live2DCanvas modelPath="src/assets/live2D/models/haru/haru_greeter_t03.model3.json" /> */}
-                {/* <Live2DCanvas modelPath="src/assets/live2D/models/huohuo/huohuo.model3.json" /> */}
-                <Live2DCanvas modelPath="src/assets/live2D/models/ariu/ariu.model3.json" />
-            </div>
+        <div className="w-fill h-fill">
+
+            <CharacterRender/>
         </div>
     )
 }
